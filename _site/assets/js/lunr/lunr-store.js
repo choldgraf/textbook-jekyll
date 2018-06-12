b3deee093cbf1b43f2e24666d40d4d3233006f1f
Chris Holdgraf <choldgraf@berkeley.edu>
2018-06-11 20:03:30 -0700

    textbook

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "First announcement!",
+        "excerpt":"      HW 11 is due Thursday, 4/19 at 11:59pm. You will receive an early submission bonus point if you turn in your final submission by Wednesday, 4/18 at 11:59pm.         The Project 3 checkpoint is due on Friday, 4/20. The project is due on Friday 4/27.   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/jupyterhub-for-education-template/first-announcements/",
+        "teaser":null},{
+        "title": "Second announcement!",
+        "excerpt":"HW 12 is due Thursday, 4/26 at 11:59pm. You will receive an early submission bonus point if you turn in your final submission by Wednesday, 4/25 at 11:59pm. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/jupyterhub-for-education-template/second-announcement/",
+        "teaser":null},{
+        "title": "Third announcement!",
+        "excerpt":"The third announcement of the class! So exciting! ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/jupyterhub-for-education-template/third-announcement/",
+        "teaser":null},{
+        "title": "The Data 8 Course Template",
+        "excerpt":"Welcome to the Data 8 course template! This is a template site that can beused to deploy Data 8 for your institution. Click the links above to accesspages for different parts of the course, which you can customize as you like. If you’d like intructions on how to fork and...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/jupyterhub-for-education-template/introduction/",
+        "teaser":null}]
